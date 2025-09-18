3f3ae88c9a50a410d07c68c1320572138a355b1e
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2025-09-18 19:56:25 +0530

    Update SingleLinkedList.py

--- Data_Structures/SingleLinkedList.py
+++ Data_Structures/SingleLinkedList.py
@@ -14,20 +14,14 @@ class SingleLinkedList:
       if self.tail != None:
          self.tail.next = node
          self.tail = node
+         self.size += 1
          return  
       self.head = node
       self.tail = node
       self.tail.next = None
+      self.size += 1   
       
    def length(self):
-        current = self.head
-        if current != None:
-            self.size += 1
-            while current.next:
-               self.size += 1
-               current = current.next
-        else:
-            return 0
         return self.size
       
    def __str__(self):
@@ -50,3 +44,4 @@ words.append(3)
 words.append(3)
 print(words)
 print(words.length())
+
