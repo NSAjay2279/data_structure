6438f912ed11ebec8b341729cd75273cc337efa5
Ajay Singh Negi <negisinghajayus@gmail.com>
2025-09-28 01:28:44 +0530

    ll.py modified with comments

--- LinkedList.py
+++ LinkedList.py
@@ -7,46 +7,55 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
-    def insert(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
+    # Time complexity = O(n)
+    # Space complexity = O(1)
+    def insert(self, data): # data = 1
+        new_node = Node(data)   # node object with data = 1, next = None
+        # node object with data = 2, next = None
+        # node object with data = 3, next = None
+        if not self.head:   # intially True
+            self.head = new_node    # head references 1st node object
             return
-        current = self.head
-        while current.next:
-            current = current.next
-        current.next = new_node
+        current = self.head # current/head -> [1, None]
+        # currrent/head -> [1, [2, None]]
+        while current.next: # false
+            current = current.next  # current = [2, None]
+        current.next = new_node # [1, [2, [3, None]]]
     
-    def delete(self, data):
-        if not self.head:
+    def delete(self, data): # data = 2
+        if not self.head:   # [1, [2, [3, None]]]
             return
         if self.head.data == data:
             self.head = self.head.next
             return
-        current = self.head
-        while current.next:
-            if current.next.data == data:
-                current.next = current.next.next
+        current = self.head # [1, [2, [3, None]]]
+        while current.next: # [2, [3, None]]
+            if current.next.data == data:   # True
+                current.next = current.next.next    # [3, None]
+                # current = [1, [2, [3, None]]] 
+                # self.head = [1, [2, [3, None]]]
+                # current.next = [3, None]
+                # current = [1, [3, None]]
                 return
             current = current.next
     
-    def search(self, data):
-        current = self.head
-        while current:
-            if current.data == data:
+    def search(self, data): # data = 2
+        current = self.head # [1, [2, [3, None]]]
+        while current:  # [2, [3, None]]
+            if current.data == data:    # 1 = 1?
                 return True
-            current = current.next
+            current = current.next  
         return False
     
     def display(self):
-        current = self.head
+        current = self.head # [1, [2, [3, None]]]
         while current:
-            print(current.data, end=" -> ")
-            current = current.next
+            print(current.data, end=" -> ") # 1 -> 2 -> 3 -> None 
+            current = current.next  # [2, [3, None]]
         print("None")
 
 if __name__ == "__main__":
-    ll = LinkedList()
+    ll = LinkedList()   # object with head = None
     ll.insert(1)
     ll.insert(2)
     ll.insert(3)
