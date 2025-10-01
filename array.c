2500cf8f8367ce718fe1d16f024f6ff53906193c
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2025-10-01 13:03:55 +0530

    Update array.c

--- array.c
+++ array.c
@@ -1,17 +1,14 @@
 #include<stdio.h>
 
-int SumOFElements(int A[])  // int A[] is int* A
-{
+int SumOFElements(int A[]) { // int A[] is int* A
   int i, sum = 0;
-  for (i =0; i<size; i++)
-  {
+  for (i =0; i<size; i++) {
     sum+= A[i];  // A[i] is *(A+i)
   }
   return sum;
 }
 
-int main()
-{
+int main() {
   int A[4];
   int size = sizeof(A)/sizeof(A[0])
   int total = SumOfElements(A, size);  // A can be used for &A[0]
