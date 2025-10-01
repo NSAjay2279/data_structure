eae7ec2f354d31cfdd53480fa7a25b9f312a51aa
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2025-10-01 13:03:06 +0530

    Fix SumOfElements function and main array handling
    Fix the SumOfElements function to accept size as a parameter and calculate the sum correctly.

--- array.c
+++ array.c
@@ -3,13 +3,18 @@
 int SumOFElements(int A[])  // int A[] is int* A
 {
   int i, sum = 0;
-  int size = sizeof(A) ...
-  ...
+  for (i =0; i<size; i++)
+  {
+    sum+= A[i];  // A[i] is *(A+i)
+  }
+  return sum;
 }
 
 int main()
 {
   int A[4];
+  int size = sizeof(A)/sizeof(A[0])
+  int total = SumOfElements(A, size);  // A can be used for &A[0]
   printf("%d\n", &A); // address of the entire array
   printf("%d\n", A);  // A decays to &A[0] (address of first element)
 }
